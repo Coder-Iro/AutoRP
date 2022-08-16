1c7d131a4671e879f0802de5e28f92abb1df7a21
iro <iro@teamif.io>
2022-08-16 17:37:34 +0900

    Add logging URL

--- src/main/java/xyz/irodev/autorp/AutoRP.java
+++ src/main/java/xyz/irodev/autorp/AutoRP.java
@@ -86,6 +86,7 @@ public final class AutoRP extends JavaPlugin implements Listener {
 
                 if (resourcePackHash == null || !resourcePackHash.equals(content[0])) {
                     resourcePackURL = MessageFormat.format("http://{0}/{1}?hash={2}", baseURL, content[1], content[0]);
+                    logger.info("URL: " + resourcePackURL);
                     resourcePackHash = content[0];
 
                     // check sha1
